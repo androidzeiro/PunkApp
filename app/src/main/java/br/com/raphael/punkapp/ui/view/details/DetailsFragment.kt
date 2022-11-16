c7d1f709066189c71a1d013e9dea1197eb393503
Raphael Amorim <raphael_amorim@outlook.com>
2022-11-16 05:22:04 -0300

    feat: Adicionado toolbar nos fragmentos

--- app/src/main/java/br/com/raphael/punkapp/ui/view/details/DetailsFragment.kt
+++ app/src/main/java/br/com/raphael/punkapp/ui/view/details/DetailsFragment.kt
@@ -2,6 +2,7 @@ package br.com.raphael.punkapp.ui.view.details
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import br.com.raphael.punkapp.databinding.FragmentDetailsBinding
 import br.com.raphael.punkapp.ui.base.BaseFragment
@@ -12,6 +13,18 @@ class DetailsFragment : BaseFragment<FragmentDetailsBinding>(FragmentDetailsBind
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        println("Beer: ${args.beer}")
+        setupLayout()
+        setupListeners()
+    }
+
+    private fun setupLayout() {
+        val beer = args.beer
+        println("Beer $beer")
+    }
+
+    private fun setupListeners() {
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 }
