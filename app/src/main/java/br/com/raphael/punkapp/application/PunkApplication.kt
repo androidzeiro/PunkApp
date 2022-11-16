27d72b62a6f88c9d268c16499ce4edf558ab190f
Raphael Amorim <raphael_amorim@outlook.com>
2022-11-16 02:36:52 -0300

    feat: Removido rotação da tela e modo escuro

--- app/src/main/java/br/com/raphael/punkapp/application/PunkApplication.kt
+++ app/src/main/java/br/com/raphael/punkapp/application/PunkApplication.kt
@@ -1,6 +1,7 @@
 package br.com.raphael.punkapp.application
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import br.com.raphael.punkapp.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
@@ -10,6 +11,7 @@ class PunkApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
