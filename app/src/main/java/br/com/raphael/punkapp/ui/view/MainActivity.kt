f859898211bfb6558b69be365a328c3322f7aec4
Raphael Amorim <raphael_amorim@outlook.com>
2022-11-16 03:40:01 -0300

    feat: Adicionado @AndroidEntryPoint no BeersFragment e MainAcitivity

--- app/src/main/java/br/com/raphael/punkapp/ui/view/MainActivity.kt
+++ app/src/main/java/br/com/raphael/punkapp/ui/view/MainActivity.kt
@@ -2,5 +2,7 @@ package br.com.raphael.punkapp.ui.view
 
 import br.com.raphael.punkapp.databinding.ActivityMainBinding
 import br.com.raphael.punkapp.ui.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate)
--- app/src/main/java/br/com/raphael/punkapp/ui/view/beers/BeersFragment.kt
+++ app/src/main/java/br/com/raphael/punkapp/ui/view/beers/BeersFragment.kt
@@ -2,5 +2,7 @@ package br.com.raphael.punkapp.ui.view.beers
 
 import br.com.raphael.punkapp.databinding.FragmentBeersBinding
 import br.com.raphael.punkapp.ui.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class BeersFragment : BaseFragment<FragmentBeersBinding>(FragmentBeersBinding::inflate)
