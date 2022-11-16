7893c01ec060a9c5bb79d28bb5d53928732df7f7
Raphael Amorim <raphael_amorim@outlook.com>
2022-11-16 05:03:41 -0300

    feat: Adicionado BeerPagingSource e BeerPagingDataAdapter

---
+++ app/src/main/java/br/com/raphael/punkapp/ui/view/adapter/BeerPagingDataAdapter.kt
@@ -0,0 +1,75 @@
+package br.com.raphael.punkapp.ui.view.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import br.com.raphael.punkapp.data.model.response.BeerResponse
+import br.com.raphael.punkapp.databinding.ItemBeerBinding
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+
+class BeerPagingDataAdapter(
+    private val clickListener: (beer: BeerResponse) -> Unit
+) : PagingDataAdapter<BeerResponse, BeerPagingDataAdapter.BeerViewHolder>(BeerDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BeerViewHolder {
+        return BeerViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: BeerViewHolder, position: Int) {
+        val item = getItem(position)
+        item?.let {
+            holder.bind(item, clickListener)
+        }
+    }
+
+    class BeerViewHolder private constructor(
+        private val binding: ItemBeerBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(beer: BeerResponse, clickListener: (beer: BeerResponse) -> Unit) {
+            binding.apply {
+                Glide
+                    .with(binding.imageBeer)
+                    .load(beer.imageUrl)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .priority(Priority.HIGH)
+                    .dontTransform()
+                    .dontAnimate()
+                    .into(binding.imageBeer)
+                textName.text = beer.name
+                textTagline.text = beer.tagline
+                buttonShowDetail.setOnClickListener {
+                    clickListener.invoke(beer)
+                }
+            }
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): BeerViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemBeerBinding.inflate(layoutInflater, parent, false)
+                return BeerViewHolder(binding)
+            }
+        }
+    }
+}
+
+class BeerDiffCallback : DiffUtil.ItemCallback<BeerResponse>() {
+    override fun areItemsTheSame(
+        oldItem: BeerResponse,
+        newItem: BeerResponse
+    ): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(
+        oldItem: BeerResponse,
+        newItem: BeerResponse
+    ): Boolean {
+        return oldItem.id == newItem.id
+    }
+}
---
+++ app/src/main/java/br/com/raphael/punkapp/ui/view/paging/BeerPagingSource.kt
@@ -0,0 +1,41 @@
+package br.com.raphael.punkapp.ui.view.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import br.com.raphael.punkapp.data.model.response.BeerResponse
+import br.com.raphael.punkapp.data.repository.BeersRepository
+
+class BeerPagingSource(
+    private val beersRepository: BeersRepository
+) : PagingSource<Int, BeerResponse>() {
+
+    override fun getRefreshKey(state: PagingState<Int, BeerResponse>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, BeerResponse> {
+        val page = params.key ?: STARTING_PAGE_INDEX
+        return try {
+            val response = beersRepository.getBeers(
+                page,
+                PER_PAGE
+            )
+
+            LoadResult.Page(
+                data = response.body() ?: mutableListOf(),
+                prevKey = if (page == STARTING_PAGE_INDEX) null else page.minus(1),
+                nextKey = if (response.body()?.isEmpty() == true) null else page.plus(1)
+            )
+        } catch (exception: Exception) {
+            return LoadResult.Error(exception)
+        }
+    }
+
+    companion object {
+        private const val STARTING_PAGE_INDEX = 1
+        const val PER_PAGE = 10
+    }
+}
