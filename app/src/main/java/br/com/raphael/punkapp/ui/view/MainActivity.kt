dc364b510972a6b073eecaffc9b49efb8fcf3f85
Raphael Amorim <raphael_amorim@outlook.com>
2022-11-16 02:41:03 -0300

    feat: Implementado a BaseActivity na MainActivity

---
+++ app/src/main/java/br/com/raphael/punkapp/ui/view/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.raphael.punkapp.ui.view
+
+import br.com.raphael.punkapp.databinding.ActivityMainBinding
+import br.com.raphael.punkapp.ui.base.BaseActivity
+
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate)
