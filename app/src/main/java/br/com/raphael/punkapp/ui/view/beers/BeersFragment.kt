3a5c7e97678f31945c5d18746956beacac1df9f5
Raphael Amorim <raphael_amorim@outlook.com>
2022-11-16 05:05:04 -0300

    feat: Adicionado recycler das cervejas e configurado o adapter no BeersFragment

--- app/src/main/java/br/com/raphael/punkapp/ui/view/beers/BeersFragment.kt
+++ app/src/main/java/br/com/raphael/punkapp/ui/view/beers/BeersFragment.kt
@@ -2,19 +2,54 @@ package br.com.raphael.punkapp.ui.view.beers
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.raphael.punkapp.databinding.FragmentBeersBinding
 import br.com.raphael.punkapp.ui.base.BaseFragment
+import br.com.raphael.punkapp.ui.view.adapter.BeerPagingDataAdapter
 import br.com.raphael.punkapp.ui.view.viewmodel.BeersViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
 class BeersFragment : BaseFragment<FragmentBeersBinding>(FragmentBeersBinding::inflate) {
 
     private val beersViewModel: BeersViewModel by viewModels()
 
+    private val beersAdapter: BeerPagingDataAdapter by lazy {
+        BeerPagingDataAdapter { beer ->
+            println(beer)
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        beersViewModel.getBeers()
+        setupLayout()
+        callApi()
+    }
+
+    private fun setupLayout() {
+        binding.recyclerBeers.layoutManager = LinearLayoutManager(context)
+        binding.recyclerBeers.adapter = beersAdapter
+
+        beersAdapter.addLoadStateListener { loadState ->
+            binding.layoutRetry.root.isVisible = loadState.refresh is LoadState.NotLoading && beersAdapter.itemCount == 0
+            binding.layoutRetry.buttonRetry.setOnClickListener {
+                beersAdapter.retry()
+            }
+            binding.progress.isVisible = loadState.refresh is LoadState.Loading && beersAdapter.itemCount == 0
+        }
+    }
+
+    private fun callApi() {
+        viewLifecycleOwner.lifecycleScope.launchWhenCreated {
+            beersViewModel.pagingFlow.collectLatest { pagingData ->
+                binding.progress.visibility = View.INVISIBLE
+                beersAdapter.submitData(pagingData)
+            }
+        }
     }
 }
