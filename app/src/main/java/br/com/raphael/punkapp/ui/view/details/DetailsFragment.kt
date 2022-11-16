35747b8c193f73503aa6e58e48306965b1ff3b9c
Raphael Amorim <raphael_amorim@outlook.com>
2022-11-16 05:49:21 -0300

    feat: Adicionado layout do detalhe da cerveja

--- app/src/main/java/br/com/raphael/punkapp/ui/view/details/DetailsFragment.kt
+++ app/src/main/java/br/com/raphael/punkapp/ui/view/details/DetailsFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import br.com.raphael.punkapp.R
 import br.com.raphael.punkapp.databinding.FragmentDetailsBinding
 import br.com.raphael.punkapp.ui.base.BaseFragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 
 class DetailsFragment : BaseFragment<FragmentDetailsBinding>(FragmentDetailsBinding::inflate) {
 
@@ -19,7 +23,32 @@ class DetailsFragment : BaseFragment<FragmentDetailsBinding>(FragmentDetailsBind
 
     private fun setupLayout() {
         val beer = args.beer
-        println("Beer $beer")
+
+        binding.apply {
+            Glide
+                .with(imageBeer)
+                .load(beer.imageUrl)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .priority(Priority.HIGH)
+                .dontTransform()
+                .dontAnimate()
+                .into(imageBeer)
+
+            textName.text = beer.name
+            textTagline.text = beer.tagline
+            textFirstBrewed.text = getString(R.string.first_brewed, beer.firstBrewed)
+            textDescription.text = getString(R.string.description, beer.description)
+            var foodPairingText = ""
+            beer.foodPairing.forEachIndexed { index, foodPairing ->
+                foodPairingText += if (index == beer.foodPairing.lastIndex) {
+                    foodPairing
+                } else {
+                    "$foodPairing\n"
+                }
+            }
+            textFoodPairing.text = getString(R.string.food_pairing, foodPairingText)
+            textBrewersTips.text = getString(R.string.brewers_tips, beer.brewersTips)
+        }
     }
 
     private fun setupListeners() {
